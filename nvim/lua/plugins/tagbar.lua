a4ac1ae7330829f84a4e73a1e69cc6ae7bdba5ee
Matthias Bidlingmeyer <m.bidlingmeyer@laserhub.com>
2022-10-20 20:26:44 +0200

    commit missing tagbar config file

---
+++ nvim/lua/plugins/tagbar.lua
@@ -0,0 +1,7 @@
+vim.g.tagbar_map_showproto = '<TAB>'
+vim.g.tagbar_autofocus = 1
+vim.g.tagbar_autoclose = 0
+vim.g.tagbar_autoclose_netrw = 1       -- close window if no buffer is left open
+vim.g.tagbar_show_tag_linenumbers = 1
+vim.g.tagbar_show_tag_count = 1
+vim.g.tagbar_sort = 0
