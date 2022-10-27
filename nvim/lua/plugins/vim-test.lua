ca2844273379ee9077e2abaf5e3c9ab526894de3
Matthias Bidlingmeyer <m.bidlingmeyer@laserhub.com>
2022-10-27 10:22:56 +0200

    add env var to test command

--- nvim/lua/plugins/vim-test.lua
+++ nvim/lua/plugins/vim-test.lua
@@ -12,5 +12,5 @@ g['test#strategy'] = "neovim"
 g['test#neovim#term_position'] = "botright"
 g['test#runner_commands'] = {'Mocha'}
 g['test#javascript#runner'] = 'mocha'
-g['test#javascript#mocha#executable'] = 'docker-compose exec lh-server mocha'
+g['test#javascript#mocha#executable'] = "docker-compose exec --env INVERSIFY_SCOPE='Transient' lh-server mocha"
 g['test#javascript#mocha#options'] = '--config server.mocharc.js'
