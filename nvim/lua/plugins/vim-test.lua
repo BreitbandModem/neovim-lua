acfd081c8302f9772d2eeb029cc7291c4370da60
Matthias Bidlingmeyer <m.bidlingmeyer@laserhub.com>
2023-02-18 12:19:58 +0100

    update test command

--- nvim/lua/plugins/vim-test.lua
+++ nvim/lua/plugins/vim-test.lua
@@ -12,5 +12,6 @@ g['test#strategy'] = "neovim"
 g['test#neovim#term_position'] = "botright"
 g['test#runner_commands'] = {'Mocha'}
 g['test#javascript#runner'] = 'mocha'
-g['test#javascript#mocha#executable'] = "docker-compose exec --env INVERSIFY_SCOPE='Transient' lh-server mocha"
+g['test#javascript#mocha#executable'] = "docker-compose exec --env INVERSIFY_SCOPE='Request' lh-server mocha"
 g['test#javascript#mocha#options'] = '--config server.mocharc.js'
+-- g['test#javascript#mocha#options'] = '--config api.mocharc.js'
