00c95f2b543a471df23e6cde84d98404f0d2df5a
Matthias Bidlingmeyer <m.bidlingmeyer@laserhub.com>
2023-02-18 12:06:13 +0100

    add mind.nvim for note taking

--- nvim/init.lua
+++ nvim/init.lua
@@ -33,4 +33,5 @@ if vim.g.vscode == nil then
   require('plugins/nvim-autopairs')
   require('plugins/tagbar')
   require('plugins/no-neck-pain')
+  require('plugins/mind')
 end
---
+++ nvim/lua/plugins/mind.lua
@@ -0,0 +1,11 @@
+require('mind').setup({
+  persistence = {
+    state_path = "~/gdrive/mind.nvim/mind.json",
+    data_dir = "~/gdrive/mind.nvim/data",
+  },
+  ui = {
+    default_icon = ' ',
+  },
+})
+    -- state_path = "/Volumes/GoogleDrive/My\\ Drive/mind.nvim/mind.json",
+    -- data_dir = "/Volumes/GoogleDrive/My\\ Drive/mind.nvim/data",
