9b97de006e2999d60fba57e8276a13c149b32600
Matthias Bidlingmeyer <m.bidlingmeyer@laserhub.com>
2023-10-05 11:23:21 +0200

    adapt to project settings

--- nvim/lua/plugins/nvim-lsp.lua
+++ nvim/lua/plugins/nvim-lsp.lua
@@ -68,6 +68,45 @@ return {
         },
       },
     }
-    require 'lspconfig'.eslint.setup {}
+    require 'lspconfig'.eslint.setup {
+      -- cmd = { "vscode-eslint-language-server", "--stdio", "--rulesdir eslint_rules", "--ext .js,.ts,.ejs" },
+      settings = {
+       options = {
+          rulePaths = {'eslint_rules'},
+        },
+        codeAction = {
+          disableRuleComment = {
+            enable = true,
+            location = "separateLine"
+          },
+          showDocumentation = {
+            enable = true
+          }
+        },
+        codeActionOnSave = {
+          enable = false,
+          mode = "all"
+        },
+        experimental = {
+          useFlatConfig = false
+        },
+        format = true,
+        nodePath = "",
+        onIgnoredFiles = "off",
+        packageManager = "npm",
+        problems = {
+          shortenToSingleLine = false
+        },
+        quiet = false,
+        rulesCustomizations = {},
+        run = "onType",
+        useESLintClass = false,
+        validate = "on",
+        workingDirectory = {
+          mode = "location"
+        }
+}
+    }
+    require'lspconfig'.vuels.setup {}
   end
 }
--- nvim/lua/plugins/vim-test.lua
+++ nvim/lua/plugins/vim-test.lua
@@ -6,8 +6,13 @@ return {
     g['test#neovim#term_position'] = "botright"
     g['test#runner_commands'] = {'Mocha'}
     g['test#javascript#runner'] = 'mocha'
-    g['test#javascript#mocha#executable'] = "docker-compose exec --env INVERSIFY_SCOPE='Request' --env LOG_LEVEL='debug' lh-server npx nyc mocha"
-    g['test#javascript#mocha#options'] = '--config server.mocharc.js'
-    -- g['test#javascript#mocha#options'] = '--config api.mocharc.js'
+
+    -- Unit Tests
+    g['test#javascript#mocha#executable'] = "docker-compose exec --env INVERSIFY_SCOPE='Request' --env LOG_LEVEL='debug' lh-server npx ts-mocha --enable-source-maps"
+    g['test#javascript#mocha#options'] = '--config tests/unit/mocharc.js'
+
+    -- API Tests
+    -- g['test#javascript#mocha#executable'] = "docker-compose exec --env INVERSIFY_SCOPE='Request' --env LOG_LEVEL='debug' --env CONFIG_ENV=local lh-server npx mocha"
+    -- g['test#javascript#mocha#options'] = '--config tests/api/mocharc.js'
   end
 }
