6dd1d9541dd89c6dbfe7664278ebc9788826b755
Matthias Bidlingmeyer <m.bidlingmeyer@laserhub.com>
2024-06-20 17:25:28 +0200

    switch config env for tests

--- nvim/lua/plugins/vim-test.lua
+++ nvim/lua/plugins/vim-test.lua
@@ -8,11 +8,11 @@ return {
     g['test#javascript#runner'] = 'mocha'
 
     -- Unit Tests
-    g['test#javascript#mocha#executable'] = "docker-compose exec --env INVERSIFY_SCOPE='Request' --env LOG_LEVEL='debug' lh-server npx ts-mocha --enable-source-maps"
+    g['test#javascript#mocha#executable'] = "docker-compose exec --env INVERSIFY_SCOPE='Request' --env LOG_LEVEL='debug' --env CONFIG_ENV=integration lh-server npx ts-mocha --enable-source-maps"
     g['test#javascript#mocha#options'] = '--config tests/unit/mocharc.js'
 
     -- API Tests
-    -- g['test#javascript#mocha#executable'] = "docker-compose exec --env INVERSIFY_SCOPE='Request' --env LOG_LEVEL='debug' --env CONFIG_ENV=local lh-server npx mocha"
+    -- g['test#javascript#mocha#executable'] = "docker-compose exec --env INVERSIFY_SCOPE='Request' --env LOG_LEVEL='debug' --env CONFIG_ENV=test lh-server npx mocha"
     -- g['test#javascript#mocha#options'] = '--config tests/api/mocharc.js'
   end
 }
