94fb45d37fc193b6a79eee1441658591deea63b3
Matthias Bidlingmeyer <m.bidlingmeyer@laserhub.com>
2024-06-20 17:28:42 +0200

    add sql client plugin

---
+++ nvim/lua/plugins/sql-client.lua
@@ -0,0 +1,18 @@
+-- connection details are persisted here: ~/.local/share/db_ui/connections.json
+return {
+  'kristijanhusak/vim-dadbod-ui',
+  dependencies = {
+    { 'tpope/vim-dadbod', lazy = true },
+    { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+  },
+  cmd = {
+    'DBUI',
+    'DBUIToggle',
+    'DBUIAddConnection',
+    'DBUIFindBuffer',
+  },
+  init = function()
+    -- Your DBUI configuration
+    vim.g.db_ui_use_nerd_fonts = 1
+  end,
+}
